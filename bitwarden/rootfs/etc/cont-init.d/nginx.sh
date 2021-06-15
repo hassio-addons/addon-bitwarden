e434fa4e07f447812b3c1378c6d860a8b0bedc93
Mandy Schoep <mandyschoep@gmail.com>
2021-06-15 17:37:25 +0200

    Add HTTP port

--- bitwarden/rootfs/etc/cont-init.d/nginx.sh
+++ bitwarden/rootfs/etc/cont-init.d/nginx.sh
@@ -12,9 +12,7 @@ if bashio::config.true 'ssl'; then
     certfile=$(bashio::config 'certfile')
     keyfile=$(bashio::config 'keyfile')
 
-    mv /etc/nginx/servers/direct-ssl.disabled /etc/nginx/servers/direct.conf
-    sed -i "s#%%certfile%%#${certfile}#g" /etc/nginx/servers/direct.conf
-    sed -i "s#%%keyfile%%#${keyfile}#g" /etc/nginx/servers/direct.conf
-else
-    mv /etc/nginx/servers/direct.disabled /etc/nginx/servers/direct.conf
+    mv /etc/nginx/servers/direct-ssl.disabled /etc/nginx/servers/direct-ssl.conf
+    sed -i "s#%%certfile%%#${certfile}#g" /etc/nginx/servers/direct-ssl.conf
+    sed -i "s#%%keyfile%%#${keyfile}#g" /etc/nginx/servers/direct-ssl.conf
 fi
